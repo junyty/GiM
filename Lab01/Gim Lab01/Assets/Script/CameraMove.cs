8a67476f4ca6c10f844b93e9d2f417e76f3a76fe
junyty <stepien.work@gmail.com>
2019-06-06 00:07:34 +0200

    test junyty

---
+++ Lab01/Gim Lab01/Assets/Script/CameraMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject player;
+    private Vector3 offset;
+
+    public void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+
+    public void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+
+}
---
+++ Lab01/Gim Lab01/Assets/Script/PlayerMove.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed;
+
+   public Text scoreText;
+   public Text WinText;
+    public Text timeText;
+
+    
+
+    private float seconds;
+
+
+
+    private int score;
+
+
+    private Rigidbody rb;
+    private GameObject MoveComponent;
+
+    void Start()
+    {
+       
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+       
+        float moveHor = Input.GetAxis("Horizontal");
+        float moveVer = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHor, 0, moveVer);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        seconds += Time.deltaTime;
+        
+        timeText.text = ("TIME " + Mathf.Round(seconds).ToString());
+
+    }
+
+   
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUpV1"))
+        {
+            score = score + 1;
+            Destroy(other.gameObject);
+            SetScore();
+
+        }
+
+        if (other.gameObject.CompareTag("PickUpV2"))
+        {
+            score = score + 3;
+            Destroy(other.gameObject);
+            SetScore();
+            
+        }
+
+
+
+    }
+
+    public void SetScore()
+    {
+        scoreText.text = ("SCORE: " + score.ToString());
+
+        if (score >= 9)
+        {
+            WinText.text = ("Wygrałes!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            GetComponent<PlayerMove>().enabled = false;
+        }
+    }
+}
+
